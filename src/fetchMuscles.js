aa2ebac6aafb2b839d37b9d3b4eb519f541513ee
jacobmmm <jacob.42193@gmail.com>
2024-11-20 14:33:24 +1100

    Extracting muscle and submuscle

--- src/fetchMuscles.js
+++ src/fetchMuscles.js
@@ -7,20 +7,59 @@ const getMuscles = async (event) => {
   const attributeName  = 'muscle_group_name'
 
   let muscles;
+  let sub_muscles;
   let results;
   try{
         results = await dynamodb.scan({TableName:"MuscleGroup"}).promise()
+        results_subgroup = await dynamodb.scan({TableName:"MuscleSubGroup"}).promise()
         muscles = results.Items
+        sub_muscles = results_subgroup.Items
     }catch (error) {
         console.log(error)
     }
 
     console.log("Muscle Object: ",muscles)
+    console.log("SubMuscle Object: ",sub_muscles)
+
+    let i=0;
+    let muscle_id=0x1A;
+    let muscle_name=" ";
+    let muscle_det=[]
+
+    while(i < muscles.length){
+
+      muscle_id=muscles[i].muscle_group_id;
+      muscle_name=muscles[i].muscle_group_name;
+
+      let j=0;
+      let submuscles=[]
+
+      while( j < sub_muscles.length ){
+        if(muscle_id == sub_muscles[j].muscle_group_id){
+          submuscles.push(sub_muscles[j].muscle_subgroup_name)
+        }
+        j=j+1
+      }
+
+      let muscleObject={[muscle_name]: submuscles}
+      console.log("Muscle Object for ",muscle_name,": ",muscleObject)
+      muscle_det.push(muscleObject)
+
+      i=i+1;
+
+      
+
+
+    }
+
+    console.log("Final Muscle Detail Object Array ",muscle_det)
   
     const muscleNames = results.Items.map(item => item[attributeName]);
     console.log(muscleNames)
 
-  const outputs = {muscles:muscleNames}
+  // const outputs = {muscles:muscleNames}
+
+    const outputs = {muscles:muscle_det}
 
 
 
